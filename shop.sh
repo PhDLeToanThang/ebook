dc7ca3a996c031e2c4cf47163a90095bfce53fdf
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2023-10-08 09:46:22 +0700

    Update shop.sh

--- shop.sh
+++ shop.sh
@@ -31,7 +31,7 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update -y
 sudo apt install php8.0-fpm php8.0-common php8.0-mbstring php8.0-xmlrpc php8.0-soap php8.0-gd php8.0-xml php8.0-intl php8.0-mysql php8.0-cli php8.0-mcrypt php8.0-ldap php8.0-zip php8.0-curl php8.0-bz2 -y
-sudo apt install mysql-server mysql-client -y
+sudo apt install mysql-server mysql-client libssl-dev -y
 
 sudo systemctl stop mysql.service 
 sudo systemctl start mysql.service 
@@ -74,22 +74,56 @@ dotnet --list-runtimes
 #Microsoft.AspNetCore.App 6.0.3 [/usr/share/dotnet/shared/Microsoft.AspNetCore.App]
 #Microsoft.NETCore.App 6.0.3 [/usr/share/dotnet/shared/Microsoft.NETCore.App]
 
-#Step 3: Install nopCommerce
+#Step 3: Configure Nginx
+sudo apt install nginx -y
+
+# xoa cau hinh nginx site default
+sudo rm -rf /etc/nginx/sites-available/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/conf.d/$FQDN.conf
+
+#Create a new Nginx virtual host file.
+#Open and edit the file.
+echo 'server {' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'listen 80;' >> /etc/nginx/sites-available/$FQDN.conf
+echo '    listen [::]:80;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'server_name '${FQDN}';' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'location / {' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_pass         http://localhost:5000;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_http_version 1.1;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_set_header   Upgrade $http_upgrade;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_set_header   Connection keep-alive;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_set_header   Host $host;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_cache_bypass $http_upgrade;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/sites-available/$FQDN.conf
+echo 'proxy_set_header   X-Forwarded-Proto $scheme;' >> /etc/nginx/sites-available/$FQDN.conf
+echo '	}' >> /etc/nginx/sites-available/$FQDN.conf
+echo '}' >> /etc/nginx/sites-available/$FQDN.conf
+#Save and close the file.
+#Test Nginx for errors.
+sudo nginx -t
+
+#Enable the configuration by creating a symlink to sites-enabled directory. 
+sudo ln -s /etc/nginx/sites-available/$FQDN.conf /etc/nginx/sites-enabled/$FQDN.conf
+
+#Restart Nginx.
+sudo service nginx restart
+
+
+#Step 4: Install nopCommerce
 # Create the nopCommerce webroot directory. Replace $FQDN with your subdomain or preferred naming style.
 sudo mkdir /var/www
 sudo mkdir /var/www/$FQDN
 # Now, switch to the directory.
 cd /var/www/$FQDN
 # Then, download the latest nopCommerce stable release from its GitHub repository. In this guide, release-$Gitnopcommerceversion is installed. Consider downloading the latest file.
-#sudo wget https://github.com/nopSolutions/nopCommerce/releases/download/release-$Gitnopcommerceversion/nopCommerce_$Gitnopcommerceversion_NoSource_linux_x64.zip
-sudo wget https://github.com/nopSolutions/nopCommerce/releases/download/release-4.60.4/nopCommerce_4.60.4_NoSource_linux_x64.zip
+sudo wget https://github.com/nopSolutions/nopCommerce/releases/download/release-${Gitnopcommerceversion}/nopCommerce_${Gitnopcommerceversion}_NoSource_linux_x64.zip
 # Extract files from the Zip archive.
-#sudo unzip nopCommerce_$Gitnopcommerceversion_NoSource_linux_x64.zip
-sudo unzip nopCommerce_4.60.4_NoSource_linux_x64.zip
+sudo unzip nopCommerce_${Gitnopcommerceversion}_NoSource_linux_x64.zip
 # To save space, delete the original zip archive.
-rm nopCommerce_$Gitnopcommerceversion_NoSource_linux_x64.zip
-rm nopCommerce_4.60.4_NoSource_linux_x64.zip
+rm nopCommerce_${Gitnopcommerceversion}_NoSource_linux_x64.zip
 # Grant Nginx (running as www-data) ownership permissions to the directory.
+sudo chmod -R 755 /var/www/$FQDN/
 sudo chown -R www-data:www-data /var/www/$FQDN/
 # All necessary files are now available on the server. Next, configure Nginx as a reverse proxy to serve these files on your subdomain.
 
@@ -101,43 +135,16 @@ sudo chown -R www-data:www-data /var/www/$FQDN/
 sudo mkdir bin
 sudo mkdir logs
 
-#Step 4: Configure Nginx
-sudo apt install nginx -y
+#Step 5: Secure the Server
+#By default, Uncomplicated Firewall (ufw) is enabled on Ubuntu 20.04, configure it to allow HTTP, HTTPS traffic on the server and block the rest.
+#Allow HTTP traffic.
+sudo ufw allow 80/tcp
+#Allow HTTPS traffic.
+sudo ufw allow 443/tcp
+#Restart the firewall
+sudo ufw reload
 
-# xoa cau hinh nginx site default
-sudo rm -rf /etc/nginx/sites-available/default
-sudo rm -rf /etc/nginx/sites-enabled/default
-rm /etc/nginx/conf.d/$FQDN.conf
-
-#Create a new Nginx virtual host file.
-#Open and edit the file.
-echo 'server {' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'listen 80;' >> /etc/nginx/conf.d/$FQDN.conf
-echo '    listen [::]:80;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'root /var/www/'${FQDN}/';'>> /etc/nginx/conf.d/$FQDN.conf
-echo 'server_name '${FQDN}';' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'location / {' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_pass         http://localhost:5000;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_http_version 1.1;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_set_header   Upgrade $http_upgrade;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_set_header   Connection keep-alive;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_set_header   Host $host;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_cache_bypass $http_upgrade;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/conf.d/$FQDN.conf
-echo 'proxy_set_header   X-Forwarded-Proto $scheme;' >> /etc/nginx/conf.d/$FQDN.conf
-echo '	}' >> /etc/nginx/conf.d/$FQDN.conf
-echo '}' >> /etc/nginx/conf.d/$FQDN.conf
-#Save and close the file.
-#Test Nginx for errors.
-sudo nginx -t
-
-#Enable the configuration by creating a symlink to sites-enabled directory. 
-sudo ln -s /etc/nginx/conf.d/$FQDN.conf /etc/nginx/sites-enabled/$FQDN.conf
-
-#Restart Nginx.
-sudo service nginx restart
-
-#Step 5: Configure nopCommerce as a Service:
+#Step 6: Configure nopCommerce as a Service:
 #To start nopCommerce, it must be running as a service on the server. To do this, create a new service file in the systemd directory.
 #Open and edit the file.
 rm /etc/systemd/system/$FQDN.service
@@ -167,21 +174,6 @@ sudo systemctl start $FQDN.service
 sudo systemctl status $FQDN.service
 #Restart Nginx to start serving nopCommerce on the subdomain.
 sudo systemctl restart nginx
-
-#Finally, visit your subdomain to start the nopCommerce setup process through a web browser.
-#http://$FQDN
-#Enter the administrator email, username, password, and country. Then, select MySQL from the drop-down list of options in the database section.
-#Under Server name, enter localhost, then enter the database name, username, and password created on step 1.
-#Click Install to proceed, nopCommerce will restart, and you may receive a 502 bad gateway error. Simply refresh your browser to log in and configure your online store.
-
-#Step 6: Secure the Server
-#By default, Uncomplicated Firewall (ufw) is enabled on Ubuntu 20.04, configure it to allow HTTP, HTTPS traffic on the server and block the rest.
-#Allow HTTP traffic.
-sudo ufw allow 80/tcp
-#Allow HTTPS traffic.
-sudo ufw allow 443/tcp
-#Restart the firewall
-sudo ufw reload
 
 #Step 7.1 Setup and Configure PhpMyAdmin
 sudo apt update -y
@@ -220,14 +212,14 @@ systemctl restart php8.0-fpm.service
 
 #Request an SSL Certificate
 #Step 8. Install Certbot.
-sudo apt install certbot python3-certbot-nginx
+#sudo apt install certbot python3-certbot-nginx
 #Request for a free Letâ€™s Encrypt SSL certificate. Replace example.com with your actual subdomain.
 
-sudo certbot --nginx -d $FQDN
+#sudo certbot --nginx -d $FQDN
 #Now, set up a new Cron job to automatically renew the SSL certificate before every 30 days of expiry.
 #Open the crontab file.
 
-sudo crontab -e
+#sudo crontab -e
 #Paste the following code:
 #0 12 * * * /usr/bin/certbot renew --quiet
 
@@ -235,3 +227,10 @@ sudo crontab -e
 #Conclusion
 #Congratulations, you have installed nopCommerce on a Ubuntu 20.04 server. 
 #You can further configure to meet your customer and visitor standards using ready-made themes, plugins, and language packs.
+
+
+#Finally, visit your subdomain to start the nopCommerce setup process through a web browser.
+#http://$FQDN
+#Enter the administrator email, username, password, and country. Then, select MySQL from the drop-down list of options in the database section.
+#Under Server name, enter localhost, then enter the database name, username, and password created on step 1.
+#Click Install to proceed, nopCommerce will restart, and you may receive a 502 bad gateway error. Simply refresh your browser to log in and configure your online store.
